6f0405bfb6f392f5da1dbf3dfba184c046273049
liushang <2317496895@qq.com>
2018-09-04 23:19:30 +0800

    采用router

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 const Koa = require('koa')  // 是个类 所以首字母大写
-const router = require('koa-router')()
+const router = require('koa-router')() // 是函数
+const bodyParser = require('koa-bodyparser')()
+const controller = require('./controller');
 const app = new Koa()
-app.use(async (ctx, next) => {
-    console.log(`Process ${ctx.request.method} ${ctx.request.url}`)
-    await next()
-    // ctx.response.type = 'text/html'
-    // ctx.response.body = '<h1>Hello,koa2!'
-})
-
-router.get('/hello/:name', async (ctx, next) => {
-    let { name } = ctx.params
-    ctx.response.body = `<h1>hello, ${name}</h1>`
-    await next()
-})
-router.get('/', async (ctx, next) => {
-    ctx.response.body = '<h1>Index</h1>'
-})
-app.use(router.routes())
-app.use(async (ctx, next) => {
-    console.log(`2222Process ${ctx.request.method} ${ctx.request.url}`)
-    ctx.response.body = '<h1>Index</h1>'
-})
+app.use(bodyParser)
+// app.use(router.routes())
+// 使用middleware:
+app.use(controller());
 app.listen(3000)
 console.log('app start at port 3000... ')
